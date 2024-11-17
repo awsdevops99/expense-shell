692fef5cd03152c8d648047fc4db7cdf27ebdc4a
om <awsdevopsom@gmail.com>
2024-11-17 22:30:50 +0530

    added the frontend

--- mysql.sh
+++ mysql.sh
@@ -8,11 +8,10 @@ cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
 echo $?
 echo -e "${colour} install the mysql server \e[0m"
 dnf install mysql-community-server -y &>>$log_file
-echo $?
+
 echo -e "${colour} start the mysql \e[0m"
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld  &>>$log_file
-echo $?
+
 echo -e "${colour} setting the password \e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-echo $?
