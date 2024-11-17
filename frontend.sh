07d314749ee90198793ff1d97e84b177ff603278
om <awsdevopsom@gmail.com>
2024-11-17 21:11:19 +0530

    added the frontend

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,14 @@
 echo -e "\e[32 Installing the Nginx \e[0m"
 dnf install nginx -y 
+echo -e "\e[32 copying the expense.conf \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
+echo -e "\e[32 removing the data \e[0m"
 rm -rf /usr/share/nginx/html/* 
+echo -e "\e[32 downloading the application \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip 
+echo -e "\e[32 extracting the application \e[0m"
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
+echo -e "\e[32 Starting the nginx \e[0m"
 systemctl enable nginx
 systemctl restart nginx
