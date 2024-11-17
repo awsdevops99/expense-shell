4c5ca3f566abc9907bce67f44d7e6b1f38a8839e
om <awsdevopsom@gmail.com>
2024-11-17 21:39:55 +0530

    added the frontend

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,17 @@
+log_file=/tmp/expense.log
+colour=\e[32m
 echo -e "\e[32m Installing the Nginx \e[0m"
-dnf install nginx -y &>> /tmp/expense.log
+dnf install nginx -y &>>$log_file
 
-echo -e "\e[32m copying the expense.conf \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf &>> /tmp/expense.log
-echo -e "\e[32m removing the data \e[0m"
-rm -rf /usr/share/nginx/html/* &>> /tmp/expense.log
-echo -e "\e[32m downloading the application \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>> /tmp/expense.log
-echo -e "\e[32m extracting the application \e[0m"
-cd /usr/share/nginx/html &>> /tmp/expense.log
-unzip /tmp/frontend.zip &>> /tmp/expense.log
-echo -e "\e[32m Starting the nginx \e[0m"
-systemctl enable nginx &>> /tmp/expense.log
-systemctl restart nginx &>> /tmp/expense.log
+echo -e "${colour} copying the expense.conf \e[0m"
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
+echo -e "${colour} removing the data \e[0m"
+rm -rf /usr/share/nginx/html/* &>>$log_file
+echo -e "${colour} downloading the application \e[0m"
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
+echo -e "${colour} extracting the application \e[0m"
+cd /usr/share/nginx/html &>>$log_file
+unzip /tmp/frontend.zip &>>$log_file
+echo -e "${colour} Starting the nginx \e[0m"
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
