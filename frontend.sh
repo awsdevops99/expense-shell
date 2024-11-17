38109160315001beb3046d7f181d167baab15f81
om <awsdevopsom@gmail.com>
2024-11-17 21:06:19 +0530

    c

---
+++ frontend.sh
@@ -0,0 +1,9 @@
+echo -e "\e[32 Installing the Nginx \e[0m"
+dnf install nginx -y 
+cp expense.conf /etc/nginx/default.d/expense.conf
+rm -rf /usr/share/nginx/html/* 
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip 
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+systemctl enable nginx
+systemctl restart nginx
