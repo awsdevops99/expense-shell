c89ca778efbb4df1ca1b54d7717bd971b6eedab8
om <awsdevopsom@gmail.com>
2024-11-18 08:37:11 +0530

    demo1

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 log_file=/tmp/expense.log
 colour="\e[36m"
-status_check () {
+status_check() {
 if [ $? -eq 0 ]; then
    echo -e "\e[36m success \e[0m"
 else
@@ -9,17 +9,17 @@ fi
 }
 echo -e "${colour} disable mysql \e[0m"
 dnf module disable mysql -y &>>log_file 
-status_check ()
+status_check()
 echo -e "${colour} copying the repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file 
-status_check ()
+status_check()
 echo -e "${colour} Installing the mysql server \e[0m"
 dnf install mysql-community-server -y &>>log_file 
-status_check () 
+status_check() 
 echo -e "${colour} start the mysql \e[0m"
 systemctl enable mysqld &>>log_file 
 systemctl start mysqld &>>log_file   
-status_check ()
+status_check()
 echo -e "${colour} set the password \e[0m"
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>log_file 
-status_check ()
+status_check()
