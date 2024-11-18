85bcf211b5ca246e6cdd9c4d8b71288752281430
om <awsdevopsom@gmail.com>
2024-11-18 08:39:34 +0530

    demo1

--- backend.sh
+++ backend.sh
@@ -9,37 +9,37 @@ fi
 }
 echo -e "${colour} Disable node js default version \e[0m"
 dnf module disable nodejs -y &>>$log_file
-status_check()
+status_check
 echo -e "${colour} Enable nodejs \e[0m"
 dnf module enable nodejs:18 -y &>>$log_file
-status_check()
+status_check
 echo -e "${colour} Installing the nodejs \e[0m"
 dnf install nodejs -y &>>$log_file
-status_check()
+status_check
 echo -e "${colour} copy backend.service file \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-status_check() 
+status_check 
 echo -e "${colour} add application user \e[0m"
 useradd expense &>>$log_file
-status_check()
+status_check
 echo -e "${colour} creating the application directory \e[0m"
 mkdir /app &>>$log_file
-status_check() 
+status_check 
 echo -e "${colour} download application content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>$log_file 
-status_check()
+status_check
 echo -e "${colour} extract the application content \e[0m"
 cd /app &>>$log_file
 unzip /tmp/backend.zip  &>>$log_file
-status_check()
+status_check
 echo -e "${colour} installing the npm \e[0m" 
 npm install &>>$log_file
-status_check()
+status_check
 echo -e "${colour} installing the client software \e[0m" 
 dnf install mysql -y &>>$log_file
-status_check() 
+status_check 
 echo -e "${colour} starting the backend services \e[0m" 
 systemctl daemon-reload &>>$log_file
 systemctl enable backend &>>$log_file
 systemctl restart backend &>>$log_file
-status_check()
+status_check
--- frontend.sh
+++ frontend.sh
@@ -10,22 +10,22 @@ fi
 }
 echo -e "${colour} Installing the Nginx \e[0m"
 dnf install nginx -y &>>$log_file
-status_check()  
+status_check 
 
 echo -e "${colour} copying the expense.conf \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-status_check()  
+status_check 
 echo -e "${colour} removing the data \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
-status_check()    
+status_check   
 echo -e "${colour} downloading the application \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
-status_check()  
+status_check 
 echo -e "${colour} extracting the application \e[0m"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
-status_check()  
+status_check 
 echo -e "${colour} Starting the nginx \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
-status_check()  
+status_check 
